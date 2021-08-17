26ca8b21c86334deaa4ee17de9d1d898e1e3a43f
ArturFleck <artur.ivanishyn@gmail.com>
2021-08-17 14:20:07 +0300

    New Code

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Some");
+        System.out.println("SOME");
+        String some;
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter:");
+        some = sc.nextLine();
+        System.out.println("You entered: " + some);
+
     }
 
 }
