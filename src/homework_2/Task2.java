bda4c0da72ed61f1e374113de93e5c55977022e2
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-08-18 21:03:16 +0300

    plus new random

--- src/homework_2/Task2.java
+++ src/homework_2/Task2.java
@@ -1,6 +1,6 @@
 package homework_2;
 
-import java.sql.Array;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task2 {
@@ -8,14 +8,25 @@ public class Task2 {
         Scanner scanner = new Scanner(System.in);
         int a, i = 0;
         double total = 0;
+
         System.out.print("How many nubmers you gonna enter? ");
         a = scanner.nextInt();
-
         while (i < a) {
             System.out.print("Enter number: ");
             total += scanner.nextDouble();
             i++;
         }
         System.out.println("The average of entered numbers is: " + total / a);
+
+        total = 0;
+        i = 0;
+
+        while (i < a) {
+            Random rand = new Random();
+            total += rand.nextInt(999);
+            i++;
+        }
+        System.out.println("\nThe average of random " + a + " numbers is: " + total / a);
+
     }
 }
