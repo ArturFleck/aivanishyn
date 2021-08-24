b10a1a356598fcaf77e74108efbc4f71d8798f7f
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-08-24 10:00:30 +0300

    change git.ignore

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 import homework_2.Task1;
 import homework_2.Task2;
 import homework_2.Task3;
+import homework_3.Some;
 
 import java.util.Scanner;
 
@@ -15,14 +16,17 @@ public class Test {
         some = sc.nextLine();
         System.out.println("You entered: " + some);*/
 
-        Task1 average = new Task1();
+        /*Task1 average = new Task1();
         average.averageNumbers();
 
         Task2 average2 = new Task2();
         average2.averageArray();
 
         Task3 account = new Task3();
-        account.banking();
+        account.banking();*/
+
+        //Some some = new Some();
+        //System.out.println(some.isNumberExist("  +3806712-34 -567"));
 
     }
 }
