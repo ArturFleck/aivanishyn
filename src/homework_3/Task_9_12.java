f042fcb40b0e84f269c38ed157f116930e073c81
ArturFleck <artur.ivanishyn@gmail.com>
2021-08-25 11:37:25 +0300

    Task 9-12 are done

---
+++ src/homework_3/Task_9_12.java
@@ -0,0 +1,68 @@
+package homework_3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task_9_12 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of array: ");
+        int[] array = new int[scan.nextInt()];
+        double counter = 0;
+        System.out.println("The array is:");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(99);
+            System.out.print(array[i] + " ");
+            counter += array[i];
+        }
+        System.out.println("\nThe average of array is: " + counter / array.length); // Task 12
+
+        /*// Task 9
+        int min = array[0];
+        int indexTemp1;
+        for (int j : array) {
+            if (min > j) {
+                min = j;
+            }
+        }
+        System.out.println("\n\nThe smallest number of array is: " + min);
+
+        //Task 10
+        int max = array[0];
+        int indexTemp2;
+        for (int j : array) {
+            if (max < j) {
+                max = j;
+            }
+        }
+        System.out.print("The largest number of array is: " + max);*/
+
+        //Combined Tasks 9-11
+        int min = array[0];
+        int max = array[0];
+        int indexMin = 0;
+        int indexMax = 0;
+        for (int j = 0; j < array.length; j++) {
+            if (min > array[j]) {
+                min = array[j];
+                indexMin = j;
+            }
+            if (max < array[j]) {
+                max = array[j];
+                indexMax = j;
+            }
+        }
+        System.out.println("\nThe smallest number of array is: " + min /*+ " at position: " + indexMin*/);
+        System.out.print("The largest number of array is: " + max /*+ " at position: " + indexMax*/);
+
+        array[indexMin] = max;
+        array[indexMax] = min;
+
+        System.out.println("\n\nChanging positions min <> max\nArray after changing:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
