da1eae16c2dce29713932ad08c6916c5a43e1bb9
Artur_Ivanishyn <artur.ivanishyn@gmail.com>
2021-08-30 20:23:24 +0300

    lesson 6 1

--- src/lesson_6/Calculator.java
+++ src/lesson_6/Calculator.java
@@ -8,35 +8,16 @@ public class Calculator {
 
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
-        calculate(str);
-
-
+        System.out.println(calculate(str));
     }
 
     static double calculate(String str) {
         String[] words = str.split(" ");
         double res = 0;
-        double a=0;
-        double b=0;
-        for (String word : words) {
-            a = Double.parseDouble(words[0]);
-            b = Double.parseDouble(words[2]);
-            System.out.println(word);
+
+        if (words[1].equals("/") ) {
+            res = Double.parseDouble(words[0])/ Double.parseDouble(words[2]);
         }
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(words[1]);
-        String st = "/";
-        System.out.println();
-        if (words[1].equals(st) ) {
-            System.out.println(a);
-            System.out.println(b);
-            res = a / b;
-            System.out.println(res);
-        }
-
-
         return res;
     }
 
