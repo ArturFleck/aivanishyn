3f425497b6c1021ac6b4055099a31f8352f4a426
ArturFleck <artur.ivanishyn@gmail.com>
2021-10-04 11:48:22 +0300

    Homework 13 DONE

--- src/homework_13/Task_3.java
+++ src/homework_13/Task_3.java
@@ -1,12 +1,16 @@
 package homework_13;
 
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
 public class Task_3 {
     public static void main(String[] args) {
-        String str="Some words gonna be written in this words document";
+        String str="Some words gonna be written in this document of unknown words. But in here, there were no other words written and no other word has to be spoken. And some other characters will be used.";
+        str = str.toLowerCase(Locale.ROOT);
+        str = str.replace(".","");
+        str = str.replace(",","");
         String []arr =str.split(" ");
 
         Map<String, Integer> map = new LinkedHashMap<>();
