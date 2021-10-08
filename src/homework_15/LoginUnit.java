8682a709d78f1f35b4f8160932d5289968febbec
ArturFleck <artur.ivanishyn@gmail.com>
2021-10-08 16:17:15 +0300

    Homework 15 start

--- src/homework_15/LoginUnit.java
+++ src/homework_15/LoginUnit.java
@@ -5,22 +5,36 @@ import java.util.Map;
 
 public class LoginUnit {
 
-    private static Map<String, String> loginData = new HashMap<>();
-        static{
-            loginData.put("WhiteOcean","qwe123456");
-            loginData.put("drakula","asdqwe123");
-        }
-        static boolean isUserAuthentic(String login, String password, String confirmPassword){
-            isLoginValid(login);
-            isPasswordValid(password);
-            return false;
-        }
-        private static void isLoginValid(String login){
+    private static String passCheck;
+    private static Map<String, String> loginData = new HashMap<>(); // key - логин, value - пароль
 
+    static {
+        loginData.put("WhiteOcean", "qwe123456");
+        loginData.put("dragula", "asdqwe123");
+        loginData.put("ArturFleck", "_arturfleck");
+        loginData.put("JohnDou", "123asdqwe");
+        loginData.put("SunnyDayDream", "SunriceAvenue");
+        loginData.put("Logan", "asdqdddas");
+    }
+
+    static boolean isUserAuthentic(String login, String password, String confirmPassword) {
+        boolean check = true;
+
+        isLoginValid(login);
+        isPasswordValid(password);
+        if (passCheck == password){
+            check= false;
         }
+        return check;
+    }
 
-        private static void isPasswordValid(String password){
+    private static void isLoginValid(String login) {
+        //System.out.println(loginData.containsKey(login));
+        passCheck = loginData.get(login);
 
-        }
+    }
 
+    private static void isPasswordValid(String password) {
+        System.out.println(loginData.containsValue(password));
+    }
 }
--- src/homework_15/TestLoginUnit.java
+++ src/homework_15/TestLoginUnit.java
@@ -3,5 +3,6 @@ package homework_15;
 public class TestLoginUnit {
     public static void main(String[] args) {
         System.out.println(LoginUnit.isUserAuthentic("Logan","123asdqwe","123asdqwe"));
+
     }
 }
