c2881d30203e02c90dbb8dd24e3478f30b663c6b
ArturFleck <artur.ivanishyn@gmail.com>
2021-10-27 10:14:43 +0300

    Homework_21 done!

--- src/homework_21/TestThreads.java
+++ src/homework_21/TestThreads.java
@@ -9,51 +9,36 @@ public class TestThreads {
         int cycle = 20;         // how many cycles we want for each thread ?
         int boundary = 100;     // max value for +/-
 
-        Runnable task1 = () -> {
-            for (int i = 0; i < cycle; i++) {
-                if (rand.nextBoolean()) {
-                    account.replenishment(rand.nextInt(boundary));
-                } else {
-                    account.withdrawal(rand.nextInt(boundary));
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-
-        Runnable task2 = () -> {
-            for (int i = 0; i < cycle; i++) {
-                if (rand.nextBoolean()) {
-                    account.replenishment(rand.nextInt(boundary));
-                } else {
-                    account.withdrawal(rand.nextInt(boundary));
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
+        Runnable task1 = () ->fill(account,cycle,boundary);
+        Runnable task2 = () ->fill(account,cycle,boundary);
 
         System.out.println("Value in account before start: " + account.getCount());
 
-        Thread tread1 = new Thread(task1);
-        Thread tread2 = new Thread(task2);
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
 
-        tread1.setName("TH-1");
-        tread2.setName("TH-2");
+        thread1.setName("TH-1");
+        thread2.setName("TH-2");
 
-        tread1.start();
-        tread2.start();
+        thread1.start();
+        thread2.start();
 
-        tread1.join();
-        tread2.join();
+        thread1.join();
+        thread2.join();
 
         System.out.println("Value in account after finish: " + account.getCount());
+    }
+
+    public static void fill(Account account, int cycle, int boundary){
+        Random rand = new Random();
+        for (int i = 0; i < cycle; i++) {
+            if (rand.nextBoolean()) {
+                account.replenishment(rand.nextInt(boundary));
+            } else {
+                account.withdrawal(rand.nextInt(boundary));
+                Thread.yield();
+            }
+        }
     }
 
 }
