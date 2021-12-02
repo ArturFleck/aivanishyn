8641cab9b84e35833484a5d43671422be35411f3
Artur Ivanishyn <artur.ivanishyn@gmail.com>
2021-12-02 23:31:07 +0200

    hw30 start

--- mvn/homework_30/src/main/java/com/hw30/DAO/StudentDAO.java
+++ mvn/homework_30/src/main/java/com/hw30/DAO/StudentDAO.java
@@ -4,25 +4,29 @@ import com.hw30.HibernateUtil;
 import com.hw30.entity.Student;
 import org.hibernate.Session;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 public class StudentDAO {
     //public Student get(Integer id){}
 
-    public Student saveOrUpdate (Student student){
+    public Student ooo (Student student){
         Session session = HibernateUtil.getSessionFactory().openSession();
+        ResultSet rs = (ResultSet) session.createQuery("select * from student");
         session.beginTransaction();
 
-        String fName = student.getFirstName();
+        session.saveOrUpdate(student);
+        System.out.println(rs);
+        /*String fName = student.getFirstName();
         String lName = student.getLastName();
 
-
-
-        session.save(student);
+        rs = session.createQuery("SELECT a FROM Student");
+        session.save(student);*/
         session.getTransaction().commit();
         HibernateUtil.shutdown();
         return null;
     }
+
 
     //public List<Student> getAll(){}
 
--- mvn/homework_30/src/main/java/com/hw30/Main.java
+++ mvn/homework_30/src/main/java/com/hw30/Main.java
@@ -4,7 +4,10 @@ import com.hw30.DAO.StudentDAO;
 import com.hw30.DAO.StudyGroupDAO;
 import com.hw30.entity.Student;
 import com.hw30.entity.StudyGroup;
+import org.hibernate.Query;
 import org.hibernate.Session;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,16 +19,24 @@ public class Main {
         StudyGroupDAO studyGroupDAO = new StudyGroupDAO();
 
         StudyGroup sg = new StudyGroup();
-        sg.setGroupName("AA-074");
+        sg.setGroupName("AI-108");
 
         Student st = new Student();
-        st.setFirstName("Alex")
-                        .setLastName("Band")
-                                .setGroupId(1)
-                                        .setYearOfAdmission(2007);
+        st.setFirstName("Chad")
+                .setLastName("Gray")
+                .setGroupId(3)
+                .setYearOfAdmission(2010);
 
-        session.save(sg);
-        session.save(st);
+        //session.save(sg);
+        //session.save(st);
+
+
+
+        Query query = session.createQuery("FROM student where id=2");
+        List list = query.list();
+
+        System.out.println(list);
+
         session.getTransaction().commit();
         HibernateUtil.shutdown();
     }
