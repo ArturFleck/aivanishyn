609108eaf42aa5f98cb434fbf71ad805b758e4a7
ArturFleck <artur.ivanishyn@gmail.com>
2021-12-02 11:04:58 +0200

    hw30 start

--- mvn/homework_30/src/main/java/com/hw30/DAO/StudentDAO.java
+++ mvn/homework_30/src/main/java/com/hw30/DAO/StudentDAO.java
@@ -1,17 +1,31 @@
 package com.hw30.DAO;
 
+import com.hw30.HibernateUtil;
 import com.hw30.entity.Student;
+import org.hibernate.Session;
 
 import java.util.List;
 
 public class StudentDAO {
-/*
-    public Student get(Integer id){}
+    //public Student get(Integer id){}
 
-    public Student saveOrUpdate (Student student){}
+    public Student saveOrUpdate (Student student){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
 
-    public List<Student> getAll(){}
+        String fName = student.getFirstName();
+        String lName = student.getLastName();
 
-    public List<Student> findByNameContaining(String partOfFullName){}*/
+
+
+        session.save(student);
+        session.getTransaction().commit();
+        HibernateUtil.shutdown();
+        return null;
+    }
+
+    //public List<Student> getAll(){}
+
+    //public List<Student> findByNameContaining(String partOfFullName){}
 
 }
