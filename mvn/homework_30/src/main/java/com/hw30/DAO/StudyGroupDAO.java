5f72554c29221b22a096e6cdd1787c663af9e1be
ArturFleck <artur.ivanishyn@gmail.com>
2021-12-03 16:06:26 +0200

    hw30 start

--- mvn/homework_30/src/main/java/com/hw30/DAO/StudyGroupDAO.java
+++ mvn/homework_30/src/main/java/com/hw30/DAO/StudyGroupDAO.java
@@ -1,4 +1,15 @@
 package com.hw30.DAO;
 
+import com.hw30.entity.StudyGroup;
+
+import java.util.List;
+
 public class StudyGroupDAO {
+
+/*    public StudyGroup get (Integer id){}
+
+    public List<StudyGroup> getAll(){}
+
+    public StudyGroup saveOrUpdate(StudyGroup studyGroup){}*/
+
 }
