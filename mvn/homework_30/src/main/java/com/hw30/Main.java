31de2018b8a455aa7a35235ad61ac22bdd915904
ArturFleck <Amatory6!>
2021-12-05 10:32:04 +0200

    hw30 Main problems

--- mvn/homework_30/src/main/java/com/hw30/Main.java
+++ mvn/homework_30/src/main/java/com/hw30/Main.java
@@ -31,7 +31,7 @@ public class Main {
         session.getTransaction().commit();
         HibernateUtil.shutdown();*/
 
-        // Problem is HERE. I can use only one method. If I use both than nothing is work.
+        // Problems are HERE. I can use only one method. If I use both than nothing is work.
         // if I close session in StudentDAO then StudyGroupDAO.getAll() will crash.
         // I wrote @ManyToOne() at studentDao, but it creates new column. Or there is no need to create @ManyToOne() at studentDao?
         // I thought that it should show name of student group from table StudyGroup.name automatically but it shouldn't create new column
