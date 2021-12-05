ea4dd48a430d7f58f716b33c597c56be01b0fa76
ArturFleck <Amatory6!>
2021-12-05 10:32:17 +0200

    hw30 Main problems

--- mvn/homework_30/src/main/java/com/hw30/Main.java
+++ mvn/homework_30/src/main/java/com/hw30/Main.java
@@ -34,7 +34,7 @@ public class Main {
         // Problems are HERE. I can use only one method. If I use both than nothing is work.
         // if I close session in StudentDAO then StudyGroupDAO.getAll() will crash.
         // I wrote @ManyToOne() at studentDao, but it creates new column. Or there is no need to create @ManyToOne() at studentDao?
-        // I thought that it should show name of student group from table StudyGroup.name automatically but it shouldn't create new column
+        // I thought that it should show name of student group from table StudyGroup.name automatically, but it shouldn't create new column
         StudentDAO studentDAO = new StudentDAO();
         studentDAO.getAll();
 
